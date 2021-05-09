18263398e6b85ed828bf59d775b724537f046082
jaclynpgh <mannja@oregonstate.edu>
2021-05-09 17:35:30 -0400

    testing

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow_auto
 var slideIndex = 0;
 carousel();
 
 function carousel() {
     var i;
-    var x = document.getElementsByClassName("carousel");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
+    var slides = document.getElementsByClassName("carousel");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > x.length) { slideIndex = 1 }
-    x[slideIndex - 1].style.display = "block";
-    setTimeout(carousel, 3000);
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(carousel, 3000); // change every three seconds
 }
